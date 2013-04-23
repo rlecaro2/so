d11badbe8d9fb3100d87e164630d282f28692e41
Rafa <rafa@ubuntu.ubuntu-domain>
2013-04-23 13:20:11 -0400

    TOP

--- iOS.py
+++ iOS.py
@@ -6,56 +6,59 @@ from proceso import Proceso
 
 class iOS:
   
-  def __init__(self):  
-    ## Cola ready de los procesos
-    self.ready=[]
-    self.historial
+    def __init__(self):  
+        ## Cola ready de los procesos
+        self.ready=[]
+        self.historial
 
-    self.dispatcher = dp.Dispatcher()
-    #self.schedululer = sch.Scheduler
+        self.dispatcher = dp.Dispatcher()
+        #self.schedululer = sch.Scheduler
 
-  def startProcess(info):
-    pass
+    def startProcess(info):
+        pass
 
       ## Esta secuencia de bloque corresponde a cuando se hace una interrupc?on de proceso
       ## Esta secuencia de bloque corresponde a cuando se hace una interrupcion de proceso
 
-      
-
-    
-  def leerInput(self, filename):
-    stack = fm.leerInput(filename)
-    self.scheduler = sch.Scheduler(stack)
-
-  def hacerLlamada():
-    pass
-  def enviarMensaje():
-    pass
-  def verContactos():
-    pass
-  def verHistorial():
-    pass
-  def correr_juego():
-      pass
+    def hacerLlamada():
+        pass
+    def enviarMensaje():
+        pass
+    def verContactos():
+        pass
+    def verHistorial():
+        pass
+    def correr_juego():
+        pass
 
 
-  ## M?todo que inserta de forma ordena los m?todo en ready, de acuerdo con su prioridad y fecha llegada a la cola
-  ## Fecha llegada == Fecha ejecucion 
-  def Insertar_en_ColaReady(self, process):
+    ## M?todo que inserta de forma ordena los m?todo en ready, de acuerdo con su prioridad y fecha llegada a la cola
+    ## Fecha llegada == Fecha ejecucion 
+    def Insertar_en_ColaReady(self, process):
 
-      if(len(ready) == 0):
-          ready.append(process)
-      
-      else:
-      ## Esto es solo en caso que deba ir al final de la fila, para no tener que recorrerla entera
-          if(process.get_prioridad()<ready[len(self.ready)-1].get_prioridad()):
-             ready.insert(len(self.ready)-1,process)
-          
-          for i in range (1, len(self.ready)-2):
-          
-             if(process.get_prioridad()<ready[i].get_prioridad()):
+        if(len(ready) == 0):
+            ready.append(process)
 
-                 ready.insert(i,process)
+        else:
+            ## Esto es solo en caso que deba ir al final de la fila, para no tener que recorrerla entera
+            if(process.get_prioridad()<ready[len(self.ready)-1].get_prioridad()):
+                ready.insert(len(self.ready)-1,process)
+
+                for i in range (1, len(self.ready)-2):
+                    if(process.get_prioridad()<ready[i].get_prioridad()):
+                        ready.insert(i,process)
+
+
+    def top(self):
+        p = self.dispatcher.running
+        left = p.duracion - p.t_running
+        print "process - time left \n"
+        print p.nombre + " - " + str(left) + "\n"
+
+        for proc in ready:
+            left = proc.duracion - proc.t_running
+            print proc.nombre + " - " + str(left) + "\n"
+
                            
            
 
